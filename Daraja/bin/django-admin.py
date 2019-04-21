49627c547a28137b297a7edbe1501d406e3f8223
harmannkibue <harmannkibue@gmail.com>
2019-04-21 10:14:08 +0300

    Removed some files from repository

--- Daraja/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/harman/Desktop/data/Django_Projects/Daraja/my_site/Daraja/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
